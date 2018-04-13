1b232b052af49f77ec4a2fdd7eb941467e96568f
Leonardo Webster <leonardowebster15@gmail.com>
2018-04-13 09:17:59 -0300

    Corrigindo menu responsivo

--- resources/views/layouts/new_app.blade.php
+++ resources/views/layouts/new_app.blade.php
@@ -16,29 +16,27 @@
 		<a class="navbar-brand a-logo" href="#">
 			<img class="logo" src="https://www.shareicon.net/data/512x512/2015/08/10/83207_paris_512x512.png" >
 		</a>
-		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler collapsed " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class=" menu">
-			<div class="navbar-collapse collapse menu-content" id="navbarSupportedContent" style="">
-				<ul class="navbar-nav bd-navbar-nav row-fluid">
-					<li class="nav-item">
-						<a href="#" title="">Home</a>
-					</li>
-					<li class="nav-item">
-						<a href="#" title="">Turismo</a>
-					</li>
-					<li class="nav-item li-logo">
-						<img class="logo" src="https://www.shareicon.net/data/512x512/2015/08/10/83207_paris_512x512.png" >
-					</li>
-					<li class="nav-item">
-						<a href="#" title="">Culinaria</a>
-					</li>
-					<li class="nav-item">
-						<a href="#" title="">Contato</a>
-					</li>
-				</ul>
-			</div>
+		<div class="navbar-collapse collapse menu-content" id="navbarSupportedContent" style="">
+			<ul class="navbar-nav bd-navbar-nav row-fluid">
+				<li class="nav-item">
+					<a href="#" title="">Home</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" title="">Turismo</a>
+				</li>
+				<li class="nav-item li-logo">
+					<img class="logo" src="https://www.shareicon.net/data/512x512/2015/08/10/83207_paris_512x512.png" >
+				</li>
+				<li class="nav-item">
+					<a href="#" title="">Culinaria</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" title="">Contato</a>
+				</li>
+			</ul>
 		</div>
 	</nav>
 
